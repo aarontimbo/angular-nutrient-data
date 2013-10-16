ee9a6af3f0768eff6350408f61f16bea8537d859
Aaron Timbo <aaron.timbo@gmail.com>
2013-10-15 21:59:24 -0500

    Add select nutrient definition option to display food nutrient info by food and nutrient type

--- app/scripts/controllers/foodnutrient.js
+++ app/scripts/controllers/foodnutrient.js
@@ -3,12 +3,32 @@
 angular.module('nutrientDataApp')
   .controller('FoodNutrientCtrl', function ($scope, $http) {
   	$scope.foodDescription = { id: '', description: '' };
+  	$scope.nutrientDefinition = { id: '203', description: 'Protein' };
+
+  	// Get nutrient definitions
+  	$http.get('http://localhost\:8090/nutrientDefinition').success(function(data, status, headers, config){
+  		$scope.nutrientDefinitions = data;
+  	});
   	$scope.results = {};
 
+  	function updateResults(data) {
+  		$scope.results = data;	
+  	} 
+
+  	function hasErrors(data) {
+  		$scope.results.errors = data;	
+  	} 
+
+  	$scope.updateNutrientInfo = function() {
+  		if ($scope.foodDescription) {
+  			$scope.searchNutrients();
+  		}
+  	}
+
   	$scope.searchNutrients = function() {
-	    $http.get('http://localhost\:8090/nutrient/?foodId=' + $scope.foodDescription.id + '&definitionId=203').success(function(data){
-	    	$scope.results = data;	
-	    });
+	    $http.get('http://localhost\:8090/nutrient/?foodId=' + $scope.foodDescription.id + '&definitionId=' + $scope.nutrientDefinition.id)
+	    	.success(updateResults)
+	    	.error(hasErrors);
   	}
     
   });
